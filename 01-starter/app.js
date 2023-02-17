720a2398c9708b24057cb720491b94d33038e592
giangbb <giangtpu@gmail.com>
2023-02-17 21:19:27 +0700

    init src code

--- 01-starter/app.js
+++ 01-starter/app.js
@@ -1,22 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     console.log(req.url);
-
-    const pathname = req.url;
+    const pathname =req.url;
     if (pathname === '/overview'){
-        res.end('THIS IS OVERVIEW PAGE');
+        //NOTE: DONT LOCK EVENT LOOP BY SYNCHRONOUS READING -> USING ASYNCHRONOUS INSTEAD
+        // fs.readFileSync();
+        fs.readFile('./templates/overview.html','utf-8', (err, data) => {
+            res.writeHead(200,
+                {
+                    'Content-type': 'text/html',
+                });
+            res.end(data);
+        });
     }else if (pathname === '/product'){
-        res.end('THIS IS PRODUCT PAGE');
-    }else{
-        res.writeHead(404, {    //not found
-            'Content-type': 'text/html',
-        });//set response with status code = 404 and header with 'Content-type' = 'text/html'
-
-        res.end('<h1>Page not found!!!!</h1>')
+        fs.readFile('./templates/product.html','utf-8', (err, data) => {
+            res.writeHead(200,
+                {
+                    'Content-type': 'text/html',
+                });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404,
+            {
+                'Content-type': 'text/html',
+            }); //set res with status code = 404 and header with 'Content-type= tex/html'
+        res.end('<h1>Page not found</h1>')
     }
 });
 
-server.listen(8080, () => {
-    console.log('Listening on 8080 ...');
+server.listen(8080,()=>{
+    console.log('Listening on 8080...');
 });
