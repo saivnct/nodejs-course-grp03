bd6caae395d2ca98bece2c2e26dafe74bfeb4fec
giangbb <giangtpu@gmail.com>
2023-02-19 21:18:35 +0700

    finish farm

--- 01-starter/app.js
+++ 01-starter/app.js
@@ -16,11 +16,14 @@ const replaceTemplate = (template, product) => {
     //using replace by regular expression  - g means global
     //template => String
     //product => JS Object
-    let output = template.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%ID%}/g,product.id);
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
 
     if (!product.organic){
         output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
@@ -82,13 +85,13 @@ const server = http.createServer((req,res) => {
     }
     else if (pathname === '/product'){
         const id = query.id * 1;
-        console.log('id',id);
-
+        const product = dataArr[id];
         res.writeHead(200,
         {
             'Content-type': 'text/html',
         });
-        res.end(templateProduct);
+        let output = replaceTemplate(templateProduct, product);
+        res.end(output);
     }else if (pathname === '/api'){
         res.writeHead(200,
             {
