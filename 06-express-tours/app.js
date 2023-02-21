491419e3c81c56ba0b2a09731b6fb16067e1914a
anhndv <dvanh4@gmail.com>
2023-02-21 21:10:46 +0700

    new commit

--- 06-express-tours/app.js
+++ 06-express-tours/app.js
@@ -1,50 +1,48 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+const data = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`,'utf-8')
+const dataArr = JSON.parse(data);
 
-
-const indexHandler = (req, res) => {
+app.get('/',(req, res) => {
     // res
     //     .status(200)
-    //     .send('Hello from server !!!');
+    //     .send('Hello form server :<');
 
     let person = {
-        name: 'abc',
-        age: 18
+        name: 'va',
+        age: 23
     }
+    res.status(200).json(person);
+})
 
+app.get('/getAllTour',(req,res) => {
     res
         .status(200)
-        .json(person);
-}
-
-const getAllToursHanlder = (req, res) => {
+        .send(dataArr)
+});
+app.get('/getTourById/:id',(req,res) => {
+//    console.log(req.params.id);
+    const id = req.params.id;
+    const dataGet = dataArr[id];
+    console.log('Data get by Id',dataGet);
     res
         .status(200)
-        .send('getAllTours');
-}
-
-const getTourById = (req, res) => {
-    console.log(req.params);
-
+        .send(dataGet)
+});
+app.get('/deleteById/:id',(req,res) => {
+    console.log('Id delete', req.params.id);
+    const id = req.params.id;
+    const dataDel = dataArr[id];
+    delete dataArr[id];
+    fs.writeFileSync('./dev-data/data/tours-simple.json', JSON.stringify(dataArr))
+    console.log('Data delete by Id',dataDel);
+    console.log('Data get by Id',dataArr);
     res
         .status(200)
-        .send('getTourById');
-}
+        .send(`Delete id: ${id} successfully!`)
 
-
-
-
-
-app.get('/',indexHandler);
-
-app.get('/getAllTours',getAllToursHanlder);
-
-app.get('/getTourById/:id',getTourById);
-
-
-
-
-app.listen(9000, () => {
-    console.log(`App running on port 9000...`);
+})
+app.listen(9000,() => {
+    console.log('App running on port 9000...');
 });
