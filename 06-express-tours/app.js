43f43eb2034889eecf7eb89a11ea7f98b527108c
giangbb <giangtpu@gmail.com>
2023-02-22 21:42:36 +0700

    combine same routes but different handlers

--- 06-express-tours/app.js
+++ 06-express-tours/app.js
@@ -8,16 +8,6 @@ const dataArr = JSON.parse(data);
 app.use(express.json());
 
 
-const indexHandler =  (req, res) => {
-    res
-        .status(200)
-        .json({
-            code: 200,
-            msg: 'OK'
-        })
-};
-
-
 const getAllTourHandler = (req,res) => {
     res
         .status(200)
@@ -103,37 +93,65 @@ const updateTourByIdHandler = (req, res) => {
     const id = req.params.id * 1;
     const updateInfo = req.body;
     const index = dataArr.findIndex((tour) => {return tour.id === id});
-    if (index >= 0){
-        const dataUpdate = dataArr[index];
 
-        console.log('Data update by Id',dataUpdate);
-
-        fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(dataArr) , (err) => {
-            res
-                .status(200)
-                .json({
-                    code: 200,
-                    msg: `Update id: ${id} successfully!`
-                });
-        })
-    }else{
-        res
-            .status(404)
+    if (index < 0){
+        return res.status(404)
             .json({
                 code: 404,
                 msg: `Not found tour with ${id}!`
             })
     }
+
+
+    const tourUpdate = dataArr[index];
+
+    // console.log('Data update by Id',tourUpdate);
+    if (updateInfo.name){
+        tourUpdate.name = updateInfo.name;
+    }
+
+
+    if (typeof updateInfo.duration === 'number' && updateInfo.duration > 0){
+        tourUpdate.duration = updateInfo.duration;
+    }
+
+    if (typeof updateInfo.maxGroupSize === 'number'  && updateInfo.maxGroupSize > 0){
+        tourUpdate.maxGroupSize = updateInfo.maxGroupSize;
+    }
+
+
+
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(dataArr) , (err) => {
+        res
+            .status(200)
+            .json({
+                code: 200,
+                msg: `Update id: ${id} successfully!`
+            });
+    })
 }
 
 
+//method 2: combine same routes but different handlers
+app.route('/api/v1/tours')
+    .get(getAllTourHandler)
+    .post(createNewTourHandler);
 
-app.get('/', indexHandler);
-app.get('/tours', getAllTourHandler);
-app.get('/tours/:id', getTourByIdHandler);
-app.delete('/tours/:id', deleteByIdHandler);
-app.post('/tours', createNewTourHandler);
-app.patch('/tour/:id',updateTourByIdHandler);
+
+
+
+//method 1: seperate routees
+// app.get('/api/v1/tours', getAllTourHandler);
+// app.post('/api/v1/tours', createNewTourHandler);
+// app.get('/api/v1/tours/:id', getTourByIdHandler);
+// app.delete('/api/v1/tours/:id', deleteByIdHandler);
+// app.patch('/api/v1/tours/:id',updateTourByIdHandler);
+
+
+
+
+
 
 app.listen(9000,() => {
     console.log('App running on port 9000...');
