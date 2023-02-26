f6c78992e5cdd026edf1e5334563b2dcf5fca5cf
giangbb <giangtpu@gmail.com>
2023-02-26 21:56:10 +0700

    initial sql connection

--- 06-express-tours/server.js
+++ 06-express-tours/server.js
@@ -6,6 +6,19 @@ dotenv.config({
 
 const app = require('./app');
 
+const sql = require('mssql');
+const sqlConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    server: 'localhost',
+    port: 1433,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
+
 
 //START SERVER
 const PORT = process.env.PORT;
