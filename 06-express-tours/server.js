53288025dea870c1b0980ed086c7682c32a43447
giangbb <giangtpu@gmail.com>
2023-02-26 22:27:14 +0700

    implement sql appPool connection

--- 06-express-tours/server.js
+++ 06-express-tours/server.js
@@ -10,14 +10,26 @@ const sql = require('mssql');
 const sqlConfig = {
     user: process.env.MSSQL_USER,
     password: process.env.MSSQL_PASS,
-    server: 'localhost',
-    port: 1433,
-    database: process.env.MSSQL_HOST,
+    server: process.env.MSSQL_HOST,
+    port: process.env.MSSQL_PORT*1 || 1433,
+    database: process.env.MSSQL_DB,
     options: {
         encrypt: false, // for azure
         trustServerCertificate: true // change to true for local dev / self-signed certs
     }
 }
+
+//https://www.npmjs.com/package/mssql#asyncawait => Global Pool Single Instance
+const appPool = new sql.ConnectionPool(sqlConfig);
+appPool.connect()
+    .then(function(pool) {
+        console.log('SQL Connected!');
+    }).catch(function(err) {
+        console.error('Error creating connection pool', err)
+    });
+
+
+
 
 
 //START SERVER
