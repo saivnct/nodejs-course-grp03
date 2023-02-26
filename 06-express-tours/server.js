e8d944cc65cf12ce03aaa77110c3eed82e5cff57
anhndv <dvanh4@gmail.com>
2023-02-26 22:03:50 +0700

    Update env variable

--- 06-express-tours/server.js
+++ 06-express-tours/server.js
@@ -8,11 +8,11 @@ const app = require('./app');
 
 const sql = require('mssql');
 const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
+    user: process.env.MSSQL_USER,
+    password: process.env.MSSQL_PASS,
     server: 'localhost',
     port: 1433,
-    database: process.env.DB_NAME,
+    database: process.env.MSSQL_HOST,
     options: {
         encrypt: false, // for azure
         trustServerCertificate: true // change to true for local dev / self-signed certs
