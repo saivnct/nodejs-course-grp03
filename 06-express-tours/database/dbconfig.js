c52cbadc7af6c0ffd0223bf7677b4f8064148a76
giangbb <giangtpu@gmail.com>
2023-02-27 21:20:52 +0700

    implement sql appPool connection

---
+++ 06-express-tours/database/dbconfig.js
@@ -0,0 +1,13 @@
+exports.sqlConfig = {
+    user: process.env.MSSQL_USER,
+    password: process.env.MSSQL_PASS,
+    server: process.env.MSSQL_HOST,
+    port: process.env.MSSQL_PORT*1 || 1433,
+    database: process.env.MSSQL_DB,
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
+
+exports.db = {};
--- 06-express-tours/server.js
+++ 06-express-tours/server.js
@@ -3,26 +3,18 @@ dotenv.config({
     path: './config.env'
 })
 
-
 const app = require('./app');
 
 const sql = require('mssql');
-const sqlConfig = {
-    user: process.env.MSSQL_USER,
-    password: process.env.MSSQL_PASS,
-    server: process.env.MSSQL_HOST,
-    port: process.env.MSSQL_PORT*1 || 1433,
-    database: process.env.MSSQL_DB,
-    options: {
-        encrypt: false, // for azure
-        trustServerCertificate: true // change to true for local dev / self-signed certs
-    }
-}
+const dbConfig = require('./database/dbconfig');
+
+
 
 //https://www.npmjs.com/package/mssql#asyncawait => Global Pool Single Instance
-const appPool = new sql.ConnectionPool(sqlConfig);
+const appPool = new sql.ConnectionPool(dbConfig.sqlConfig);
 appPool.connect()
     .then(function(pool) {
+        dbConfig.db.pool = pool
         console.log('SQL Connected!');
     }).catch(function(err) {
         console.error('Error creating connection pool', err)
