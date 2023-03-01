b4c6dea7cecf01511536b4b575d5784f1eba5fcb
anhndv <dvanh4@gmail.com>
2023-03-01 21:13:51 +0700

    Update api

--- 06-express-tours/DAO/TourDAO.js
+++ 06-express-tours/DAO/TourDAO.js
@@ -32,3 +32,42 @@ exports.getTourById = async (id) => {
 
     return result.recordsets[0][0];
 }
+exports.deleteTourById = async (id) => {
+    if(!dbConfig.db.pool){
+        throw new Error('Not connected to db');
+    }
+    let request = dbConfig.db.pool.request();
+    let result = await request
+        .input('id', sql.Int, id)
+        .query('delete * from Tours where id = @id');
+
+    console.log(result);
+//     return result
+}
+
+exports.createNewTour = async(name, price) => {
+    if(!dbConfig.db.pool){
+        throw new Error('Not connected to db');
+    }
+    let request = dbConfig.db.pool.request();
+    let result = await request
+        .input('name', sql.varchar, name)
+        .input('price', sql.Int, price)
+        .query('insert into Tours (name, price) values (@name,@price)');
+    console.log(result);
+    // return result
+}
+exports.updateTourById = async (id, updateInfo) => {
+    if(!dbConfig.db.pool){
+        throw new Error('Not connected to db');
+    }
+    let request = dbConfig.db.pool.request();
+    let result = await request.input('id', sql.Int, id)
+        .input('price', sql.Int, price)
+        .input('rating', sql.float, rating)
+        .query('update Tours set price = @price, rating = @rating where id = @id');
+
+    console.log(result);
+    // return result
+
+}
--- 06-express-tours/controllers/tour.js
+++ 06-express-tours/controllers/tour.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const data = fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`,'utf-8');
 const dataArr = JSON.parse(data);
-
-const TourDAO = require('./../DAO/TourDAO');
+const TourDAO = require('./../DAO/TourDAO')
 
 
 exports.getAllTour = async (req,res) => {
@@ -85,101 +84,80 @@ exports.getTourById = async  (req,res) => {
 
 
 }
-
-exports.deleteById = (req,res) => {
-    console.log('Id delete', req.params.id);
+exports.deleteById = async (req,res) => {
     const id = req.params.id*1;
-
-    const index = dataArr.findIndex((tour) => {return tour.id === id});
-    if (index >= 0){
-        const dataDel = dataArr[index];
-        dataArr.splice(index,1);
-        console.log('Data delete by Id',dataDel);
-
-        fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(dataArr) , (err) => {
-            res
-                .status(200)
-                .json({
-                    code: 200,
-                    msg: `Delete id: ${id} successfully!`
-                });
-        })
-    }else{
-        res
-            .status(404)
-            .json({
-                code: 404,
-                msg: `Not found tour with ${id}!`
-            })
-    }
-}
-
-exports.createNewTour = (req,res) => {
-    console.log('createNewTour', req.body);
-    const newData = req.body;
-    dataArr.push(newData)
-
-    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(dataArr), (err) => {
-        if(!err) {
-            res
-                .status(201)
-                .json({
-                    code: 201,
-                    msg: `Add new tour successfully!`,
-                    data: newData
-                });
-        } else {
-            res
-                .status(500)
-                .json({
-                    code: 500,
-                    msg: `Add new tour fail!`
-                });
-        }
-
-    });
-}
-
-exports.updateTourById = (req, res) => {
-    console.log('Id update', req.params.id);
-    const id = req.params.id * 1;
-    const updateInfo = req.body;
-    const index = dataArr.findIndex((tour) => {return tour.id === id});
-
-    if (index < 0){
-        return res.status(404)
-            .json({
-                code: 404,
-                msg: `Not found tour with ${id}!`
-            })
-    }
-
-
-    const tourUpdate = dataArr[index];
-
-    // console.log('Data update by Id',tourUpdate);
-    if (updateInfo.name){
-        tourUpdate.name = updateInfo.name;
-    }
-
-
-    if (typeof updateInfo.duration === 'number' && updateInfo.duration > 0){
-        tourUpdate.duration = updateInfo.duration;
-    }
-
-    if (typeof updateInfo.maxGroupSize === 'number'  && updateInfo.maxGroupSize > 0){
-        tourUpdate.maxGroupSize = updateInfo.maxGroupSize;
-    }
-
-
-
-
-    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(dataArr) , (err) => {
-        res
+    let tour;
+    try {
+        tour = await TourDAO.deleteTourById(id)
+        return res
             .status(200)
             .json({
                 code: 200,
-                msg: `Update id: ${id} successfully!`
+                msg: `Delete tour with ${Id} successfully!`,
+            })
+    } catch (e) {
+        return res
+            .status(500)
+            .json({
+                code: 500,
+                msg: e
+            })
+    }
+}
+
+exports.createNewTour = async (req,res) => {
+    console.log('createNewTour', req.body);
+    const newData = req.body;
+    let tour;
+    try {
+        tour = await TourDAO.createNewTour(newData)
+        return res
+            .status(200)
+            .json({
+                code: 200,
+                msg: `Create new tour successfully!`,
+                tour
+            })
+    }catch (e){
+        res
+            .status(500)
+            .json({
+                code: 500,
+                msg: e
             });
-    })
+    }
+}
+
+exports.updateTourById = async(req, res) => {
+    console.log('Id update', req.params.id);
+    const id = req.params.id * 1;
+    const updateInfo = req.body;
+    if (updateInfo.name){
+        const name = updateInfo.name;
+    }
+
+    if (typeof updateInfo.price === 'number' && updateInfo.price > 0){
+        let price = updateInfo.price;
+    }
+
+    if (typeof updateInfo.rating === 'number' && updateInfo.price > 0){
+        let rating = updateInfo.price;
+    }
+    try {
+        tour = await TourDAO.updateTourById(id,updateInfo)
+        return res
+            .status(200)
+            .json({
+                code: 200,
+                msg: `Update id: ${id} successfully!`,
+                tour
+            })
+    }catch (e){
+        res
+            .status(500)
+            .json({
+                code: 500,
+                msg: e
+            });
+    }
 }
